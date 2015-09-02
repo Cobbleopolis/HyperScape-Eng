f85a547d3ec94469e6934d5c5efc555d40a4c671
Cobbleopolis <cobbleopolis@gmail.com>
2015-09-02 14:07:03 -0400

    Changed how Buttons generate verts

--- src/com/cobble/hyperscape/gui/GuiButton.scala
+++ src/com/cobble/hyperscape/gui/GuiButton.scala
@@ -14,13 +14,13 @@ import org.lwjgl.opengl.{GL30, GL15, GL20}
  */
 class GuiButton(text: String, x: Float = 0.0f, y: Float = 0, height: Float = 0.2f, width: Float = 0.2f) extends GuiElement {
     val verts: Array[Float] = Array(
-        x,          y,         0.0f,      0.4f, 0.0f, 0.3f, 1.0f,
-        x,          y + width, 0.0f,      0.4f, 0.0f, 0.3f, 1.0f,
-        x - height, y,         0.0f,      0.4f, 0.0f, 0.3f, 1.0f,
+        x,         y,          0.0f,      0.4f, 0.0f, 0.3f, 1.0f,
+        x + width, y + height, 0.0f,      0.4f, 0.0f, 0.3f, 1.0f,
+        x,         y + height, 0.0f,      0.4f, 0.0f, 0.3f, 1.0f,
 
-        x,          y + width, 0.0f,      0.4f, 0.0f, 0.3f, 1.0f,
-        x - height, y + width, 0.0f,      0.4f, 0.0f, 0.3f, 1.0f,
-        x - height, y,         0.0f,      0.4f, 0.0f, 0.3f, 1.0f
+        x,         y,          0.0f,      0.4f, 0.0f, 0.3f, 1.0f,
+        x + width, y,          0.0f,      0.4f, 0.0f, 0.3f, 1.0f,
+        x + width, y + height, 0.0f,      0.4f, 0.0f, 0.3f, 1.0f
     ) //new Array[Float](72)
 
     val guiModel = new GuiModel(verts)
