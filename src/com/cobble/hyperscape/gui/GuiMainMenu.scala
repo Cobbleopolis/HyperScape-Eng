1a3e52dfca3e0e312432beff6c53469e01a854ed
Cobbleopolis <cobbleopolis@gmail.com>
2015-09-14 15:27:33 -0400

    Added another button because why not

--- src/com/cobble/hyperscape/gui/GuiMainMenu.scala
+++ src/com/cobble/hyperscape/gui/GuiMainMenu.scala
@@ -18,6 +18,7 @@ class GuiMainMenu extends GuiScreen {
 //        guiButton = new GuiButton("Thing", -.5f, -.5f, 1f, 1f)
         buttonList = buttonList :+ guiButton
         buttonList = buttonList :+ new GuiButton("Thing",  -.25f, -.25f, 0.5f, 0.5f, new Vector4f(0.4f, 0.0f, 0.3f, 1.0f))
+        buttonList = buttonList :+ new GuiButton("Thing",  -.5f, -.5f, 1.0f, 1.0f, new Vector4f(0.47f, 0.11f, 0.11f, 1.0f))
     }
 
     override def render(): Unit = {
