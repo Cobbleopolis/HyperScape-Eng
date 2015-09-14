0bf958797956d16cf5c0d652c2c1aa7f24bd30ce
Cobbleopolis <cobbleopolis@gmail.com>
2015-09-14 15:23:21 -0400

    Added button to main menu gui for testing

--- src/com/cobble/hyperscape/gui/GuiMainMenu.scala
+++ src/com/cobble/hyperscape/gui/GuiMainMenu.scala
@@ -17,7 +17,7 @@ class GuiMainMenu extends GuiScreen {
         guiButton = new GuiButton("Thing", -0.125f, -0.125f, 0.25f, 0.25f)
 //        guiButton = new GuiButton("Thing", -.5f, -.5f, 1f, 1f)
         buttonList = buttonList :+ guiButton
-//        buttonList = buttonList :+ new GuiButton("Thing",  -.5f, -.5f, 1f, 1f, new Vector4f(0.4f, 0.0f, 0.3f, 1.0f))
+        buttonList = buttonList :+ new GuiButton("Thing",  -.25f, -.25f, 0.5f, 0.5f, new Vector4f(0.4f, 0.0f, 0.3f, 1.0f))
     }
 
     override def render(): Unit = {
