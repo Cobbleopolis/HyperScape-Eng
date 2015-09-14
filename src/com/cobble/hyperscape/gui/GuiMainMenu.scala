612cfd6065982ecac45356e641861952b18c7e28
Cobbleopolis <cobbleopolis@gmail.com>
2015-09-14 15:29:31 -0400

    Aligned some code

--- src/com/cobble/hyperscape/gui/GuiMainMenu.scala
+++ src/com/cobble/hyperscape/gui/GuiMainMenu.scala
@@ -9,8 +9,8 @@ class GuiMainMenu extends GuiScreen {
 
     override def initGui(): Unit = {
         buttonList = buttonList :+ new GuiButton("Thing", -0.125f, -0.125f, 0.25f, 0.25f)
-        buttonList = buttonList :+ new GuiButton("Thing",  -.25f, -.25f, 0.5f, 0.5f, new Vector4f(0.4f, 0.0f, 0.3f, 1.0f))
-        buttonList = buttonList :+ new GuiButton("Thing",  -.5f, -.5f, 1.0f, 1.0f, new Vector4f(0.47f, 0.11f, 0.11f, 1.0f))
+        buttonList = buttonList :+ new GuiButton("Thing",  -.25f,  -.25f,   0.5f,  0.5f, new Vector4f(0.4f, 0.0f, 0.3f, 1.0f))
+        buttonList = buttonList :+ new GuiButton("Thing",  -.5f,   -.5f,    1.0f,  1.0f, new Vector4f(0.47f, 0.11f, 0.11f, 1.0f))
     }
 
 //    override def render(): Unit = {
