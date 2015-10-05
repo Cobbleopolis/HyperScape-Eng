32efe01d3771bdcbbeeab70d80f4e27b8f65487c
Cobbleopolis <cobbleopolis@gmail.com>
2015-10-05 12:34:56 -0400

    Changes to the main menu gui

--- src/com/cobble/hyperscape/gui/GuiMainMenu.scala
+++ src/com/cobble/hyperscape/gui/GuiMainMenu.scala
@@ -10,8 +10,9 @@ class GuiMainMenu extends GuiScreen {
     override def initGui(): Unit = {
         val size: Float = Display.getHeight.asInstanceOf[Float] / 8f
         val halfSize: Float = size / 2f
+        val doubleSize: Float = size * 2f
         buttonList = buttonList :+ new GuiButton("Thing", -halfSize, -halfSize, size, size)
-//        buttonList = buttonList :+ new GuiButton("Thing",  -.25f,  -.25f,   0.5f,  0.5f, new Vector4f(0.4f, 0.0f, 0.3f, 1.0f))
+        buttonList = buttonList :+ new GuiButton("Thing",  -size,  -size,   doubleSize,  doubleSize, new Vector4f(0.4f, 0.0f, 0.3f, 1.0f))
 //        buttonList = buttonList :+ new GuiButton("Thing",  -.5f,   -.5f,    1.0f,  1.0f, new Vector4f(0.47f, 0.11f, 0.11f, 1.0f))
     }
 
