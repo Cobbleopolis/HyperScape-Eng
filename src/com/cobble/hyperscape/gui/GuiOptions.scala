6614796c15570bdee0e70bb05764614ecf0323bc
Cobbleopolis <agounimon@yahoo.com>
2015-10-20 20:51:20 -0400

    testing code for guis

--- src/com/cobble/hyperscape/gui/GuiOptions.scala
+++ src/com/cobble/hyperscape/gui/GuiOptions.scala
@@ -6,6 +6,7 @@ import org.lwjgl.util.vector.Vector3f
 class GuiOptions extends GuiScreen {
 
     elementList = elementList :+ new GuiButton("Go Back!", -400, -300, 800, 100, new Vector3f(1f, 0.5f, 0f))
+    elementList = elementList :+ new GuiButton("This is a different GUI", -400, 0, 800, 300, new Vector3f(0.749f, 0.211f, 0.047f))
 
     override def onClick(elementIndex: Int): Unit ={
         if (elementIndex == 0)
