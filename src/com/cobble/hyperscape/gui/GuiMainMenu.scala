71c271857dc60a2de7e517caf1ab432aeb37047a
Cobbleopolis <cobbleopolis@gmail.com>
2015-10-26 14:38:44 -0400

    Repositioned the GUI buttons

--- src/com/cobble/hyperscape/gui/GuiMainMenu.scala
+++ src/com/cobble/hyperscape/gui/GuiMainMenu.scala
@@ -6,11 +6,10 @@ import org.lwjgl.util.vector.Vector3f
 
 class GuiMainMenu extends GuiScreen("Main Menu") {
 
-    val woah = new GuiButton("WOAH A NEW BUTTON!", -500, 0, 340, 340, new Vector3f(0.141f, 0.317f, 0.709f))
+    val woah = new GuiButton("WOAH A NEW BUTTON!", -330f, 0f, 300f, 300f, new Vector3f(0.141f, 0.317f, 0.709f))
 
-    elementList = elementList :+ new GuiButton("Change that GUI!", -400, -300, 800, 100, new Vector3f(1f, 0f, 0f))
-    //        buttonList = buttonList :+ new GuiButton("Hello World!", 0, 0, 340, 340, new Vector3f(0.4f, 0.0f, 0.3f), 7)
-    elementList = elementList :+ new GuiButton("Hello World!", 0, 0, 340, 340, new Vector3f(0.4f, 0.0f, 0.3f))
+    elementList = elementList :+ new GuiButton("Change that GUI!", -330f, -300f, 660f, 100f, new Vector3f(1f, 0f, 0f))
+    elementList = elementList :+ new GuiButton("Hello World!", 0f, 0f, 300f, 300f, new Vector3f(0.4f, 0.0f, 0.3f))
 
     override def onClick(elementIndex: Int): Unit ={
         if (elementIndex == 0)
--- src/com/cobble/hyperscape/gui/GuiOptions.scala
+++ src/com/cobble/hyperscape/gui/GuiOptions.scala
@@ -5,8 +5,8 @@ import org.lwjgl.util.vector.Vector3f
 
 class GuiOptions extends GuiScreen ("Options") {
 
-    elementList = elementList :+ new GuiButton("Go Back!", -400, -300, 800, 100, new Vector3f(1f, 0.5f, 0f))
-    elementList = elementList :+ new GuiButton("This is a different GUI", -400, 0, 800, 300, new Vector3f(0.749f, 0.211f, 0.047f))
+    elementList = elementList :+ new GuiButton("Go Back!", -330f, -300f, 660f, 100f, new Vector3f(1f, 0.5f, 0f))
+    elementList = elementList :+ new GuiButton("This is a different GUI", -330f, 0f, 660f, 300f, new Vector3f(0.749f, 0.211f, 0.047f))
 
     override def onClick(elementIndex: Int): Unit ={
         if (elementIndex == 0)
