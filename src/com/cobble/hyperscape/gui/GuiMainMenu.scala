6d7e80de654c6073fbd93c391f8e2af935c5d643
Cobbleopolis <agounimon@yahoo.com>
2015-10-26 18:22:37 -0400

    Changed GUI Layout

--- src/com/cobble/hyperscape/gui/GuiMainMenu.scala
+++ src/com/cobble/hyperscape/gui/GuiMainMenu.scala
@@ -2,6 +2,7 @@ package com.cobble.hyperscape.gui
 
 import com.cobble.hyperscape.core.HyperScape
 import com.cobble.hyperscape.util.GLUtil
+import org.lwjgl.opengl.Display
 import org.lwjgl.util.vector.Vector3f
 
 class GuiMainMenu extends GuiScreen("Main Menu") {
@@ -9,13 +10,14 @@ class GuiMainMenu extends GuiScreen("Main Menu") {
 //    val woah = new GuiButton("WOAH A NEW BUTTON!", -330f, 0f, 300f, 300f, new Vector3f(0.141f, 0.317f, 0.709f))
 
     elementList = elementList :+ new GuiButton("New Game", -330f, 0f, 660f, 75f, new Vector3f(1f, 0f, 0f))
-    elementList = elementList :+ new GuiButton("Options", -330f, -300f, 660f, 75f, new Vector3f(1f, 0f, 0f))
+    elementList = elementList :+ new GuiButton("Options", -330f, -115f, 660f, 75f, new Vector3f(1f, 0f, 0f))
+    elementList = elementList :+ new GuiButton("Exit Game", -330f, -300, 660f, 75f, new Vector3f(1f, 0f, 0f))
 //    elementList = elementList :+ new GuiButton("Hello World!", 0f, 0f, 300f, 300f, new Vector3f(0.4f, 0.0f, 0.3f))
 
     override def onClick(elementIndex: Int): Unit ={
         if (elementIndex == 1)
             HyperScape.currentGameState.changeGui(Guis.guiOptions)
-//        if (elementIndex == 1)
+//        if (elementIndex == 2)
 //            if (elementList.size < 3)
 //                elementList = elementList :+ woah
 //            else
