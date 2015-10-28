eb7f955a5297e11d1977e3ad244f18e76fd1fbb4
Cobbleopolis <cobbleopolis@gmail.com>
2015-10-28 14:34:07 -0400

    Fixed an error in gui initialization

--- src/com/cobble/hyperscape/gui/Guis.scala
+++ src/com/cobble/hyperscape/gui/Guis.scala
@@ -1,6 +1,12 @@
 package com.cobble.hyperscape.gui
 
+import com.cobble.hyperscape.core.HyperScape
+import com.cobble.hyperscape.reference.Reference
+
 object Guis {
+
+    HyperScape.mainCamera.mode = Reference.Camera.ORTHOGRAPHIC_MODE
+    HyperScape.mainCamera.updatePerspective()
 
     val guiMainMenu = new GuiMainMenu
 
