4742145da70b5ca268d4d67505c19987b7e4c7cd
Cobbleopolis <agounimon@yahoo.com>
2015-11-09 20:07:56 -0500

    Added a method to upload a modelMatrix

--- src/com/cobble/hyperscape/util/RenderUtil.scala
+++ src/com/cobble/hyperscape/util/RenderUtil.scala
@@ -1,5 +1,19 @@
 package com.cobble.hyperscape.util
 
+import com.cobble.hyperscape.core.HyperScape
+import com.cobble.hyperscape.registry.ShaderRegistry
+import org.lwjgl.opengl.GL20
+import org.lwjgl.util.vector.Matrix4f
+
 object RenderUtil {
+
+    def uploadModelMatrix(modelMatrix: Matrix4f): Unit = {
+        val loc = ShaderRegistry.getCurrentShader.getUniformLocation("modelMatrix")
+        HyperScape.uploadBuffer.clear()
+        modelMatrix.store(HyperScape.uploadBuffer)
+        HyperScape.uploadBuffer.flip()
+        GL20.glUniformMatrix4(loc, false, HyperScape.uploadBuffer)
+        HyperScape.uploadBuffer.clear()
+    }
 
 }
