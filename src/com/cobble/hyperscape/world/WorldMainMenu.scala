e75ff6204b47449931b3c232cff8760efe7cc6ed
Cobbleopolis <cobbleopolis@gmail.com>
2015-11-30 14:22:33 -0500

    Changed world gen

--- src/com/cobble/hyperscape/world/WorldMainMenu.scala
+++ src/com/cobble/hyperscape/world/WorldMainMenu.scala
@@ -18,25 +18,12 @@ class WorldMainMenu extends World {
     var i = 0
 
 
-//    for (x <- 0 until 16)
-//        for (z <- 0 until 256) {
-//            val offset = z / 16
-//            for (y <- offset until (if (x % 16 > 8 && z % 16 > 8) offset + 5 else offset + 1))
-//                setBlock(x, y, z, if (rand.nextBoolean()) Blocks.blank else Blocks.thing)
-//        }
-    for (x <- 0 until 16)
-        for (z <- 0 until 16)
-            setBlock(x, 0, z, if (rand.nextBoolean()) Blocks.blank else Blocks.thing)
+    for (x <- 0 until 256)
+        for (z <- 0 until 256) {
+            val y = (x / 16) + (z / 16)
+                setBlock(x, y, z, if (rand.nextBoolean()) Blocks.blank else Blocks.thing)
+        }
 
-//    val block = chunks(0).blocks(0)
-//    val block2 = chunks(0).blocks(1)
-//    println(block)
-//    println(block2)
-    println(chunks(0).getBlock(5, 0, 0).blockID)
-    println(getBlock(5, 0, 0).blockID)
-    println(chunks(0).getBlock(0, 0, 0).blockID)
-    println(getBlock(0, 0, 0).blockID)
-    println(chunks.size)
 
 //    override def tick(): Unit = {
 //        super.tick()
