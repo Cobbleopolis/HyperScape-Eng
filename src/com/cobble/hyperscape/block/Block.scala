43f8a3ed9e2e06b23ea4588936f46ad5856ae28d
Cobbleopolis <cobbleopolis@gmail.com>
2015-12-03 12:34:39 -0500

    Added render types

--- src/com/cobble/hyperscape/block/Block.scala
+++ src/com/cobble/hyperscape/block/Block.scala
@@ -1,7 +1,12 @@
 package com.cobble.hyperscape.block
 
+import com.cobble.hyperscape.render.BlockRenderType
+import com.cobble.hyperscape.render.BlockRenderType.BlockRenderType
+
 trait Block {
     val blockID: Int
 
     val uv: (Float, Float)
+
+	var renderType: BlockRenderType = BlockRenderType.NORMAL
 }
---
+++ src/com/cobble/hyperscape/render/BlockRenderType.scala
@@ -0,0 +1,8 @@
+package com.cobble.hyperscape.render
+
+object BlockRenderType extends Enumeration {
+
+	type BlockRenderType = Value
+
+	val NORMAL, NON_FULL_BLOCK, GLASS, NONE = Value
+}
